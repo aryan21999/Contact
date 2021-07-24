278d681d7e8cee90e6bbf462e77927bde2969e1e
Aryan <test@test.com>
2021-07-24 17:57:07 +0530

    initial commit

--- routers/contact.js
+++ routers/contact.js
@@ -38,7 +38,48 @@ router.get('/contact/:id', async (req, res,) => {
         } else
         res.send(contact)
     }catch(e) {
+    console.log('their is not any contact of this id')
     res.status(500).send(e)
+    }
+})
+
+router.patch('/contact/:id', async (req, res,) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'phone', 'email']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Can Not Update!' })
+    }
+    try {
+        const contact = await Contact.findById(req.params.id)
+        if(contact.owner == req.user.email) {
+            updates.forEach((update) => contact[update] = req.body[update])
+            await contact.save()
+        }
+        else if(contact.owner != req.user.email) {
+            return res.status(400).send(e)
+        }
+        if(!contact) {
+            return res.status(404).send(e)
+        }
+        res.send(contact)
+        }  
+        catch(e) {
+            res.status(400).send(e)
+        }
+})
+
+router.delete('/contact/:id/delete', async (req, res,) => {
+    try {
+        const contact = await Contact.findOneAndDelete({ _id: req.params.id, owner: req.user.email })
+    if(!contact) {
+        return res.status(404).send()
+    }
+    res.send(contact)
+    }
+    catch (e) {
+        res.status(500),send()
     }
 })
 
