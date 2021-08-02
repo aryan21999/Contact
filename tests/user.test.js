4237d298fe8284cdbe592efe54bb2c8529834b53
Aryan <test@test.com>
2021-08-02 12:24:37 +0530

    initial commit

--- tests/user.test.js
+++ tests/user.test.js
@@ -4,6 +4,7 @@ const User = require('../models/user')
 const Contact = require('../models/contact')
 const db = require('../db/mongoose')
 
+
 const userOne = {
     name: "Aryan12",
     email: "test12@test.com",
@@ -25,11 +26,63 @@ test('SignUp a new user', async () => {
     .expect(201)
 })
 
-test('login user', async () => {
+test('login that user', async () => {
     const response = await request(app).post('/users/login').send({
         email: userOne.email,
         password: userOne.password
     })
     .expect(200)
     .expect((res) => {token = res.body.token, user = res.body.user})
+})
+
+const contactOne = {
+    name: "lucky",
+    phone: "1234432156",
+    email: "lucky@test.com"
+}
+
+const contactTwo = {
+    name: "Ayush",
+    phone: "0987789065",
+    email: "ayush@test.com"
+}
+
+var id
+
+test('Create Contact', async () => {
+    await request(app).post('/contact')
+    .set('Authorization', `Bearer ${token}`)
+    .send({
+        name: contactOne.name,
+        phone: contactOne.phone,
+        email: contactOne.email
+    })
+    .expect((res) => {id = res.body._id})
+    .expect(201)
+})
+
+test('Update Contact', async () => {
+    await request(app).patch(`/contact/${id}`)
+    .set('Authorization', `Bearer ${token}`)
+    .send({
+        name: contactTwo.name,
+        phone: contactTwo.phone,
+        email: contactTwo.email
+    })
+    .expect(200)
+})
+
+test('Delete Contact', async () => {
+    await request(app)
+    .delete(`/contact/${id}/delete`)
+    .set('Authorization', `Bearer ${token}`)
+    .send()
+    .expect(200);
+})
+
+test('Read all contact', async () => {
+    const response = await request(app).get('/contact')
+    .set('Authorization', `Bearer ${token}`)
+    .expect(200)
+    expect(response.body[0].name).toEqual(contactOne.name)
 })
